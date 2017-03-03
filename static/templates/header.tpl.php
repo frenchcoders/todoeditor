9adeb7fed70089b9f27745b07a9ca0901a71ad32
Thomas LHTS <contact@thts.fr>
2017-03-03 07:04:17 +0100

    Script FontAwesome chargé depuis leur CDN

--- static/templates/header.tpl.php
+++ static/templates/header.tpl.php
@@ -4,6 +4,10 @@
 		<title><?php echo isset($_GET['id']) ? 'Afficher une liste de tâches':'Listes de tâches'; ?></title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Font awesome -->
+		<script src="https://use.fontawesome.com/96f1347813.js"></script>
+
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
@@ -11,7 +15,6 @@
 
 
 		<!-- CSS RULES -->
-		<link href="https://fonts.googleapis.com/css?family=Raleway:200" rel="stylesheet">
 		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" type="text/css">
 		<link href="static/css/rules.css" rel="stylesheet" />
 
